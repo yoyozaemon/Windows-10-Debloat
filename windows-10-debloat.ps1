c78b67facc09895a56a20faeb23db9c84741cfe0
Vijay J <81014244+yoyozaemon@users.noreply.github.com>
2022-05-22 10:39:11 +0530

    Update windows-10-debloat.ps1

--- windows-10-debloat.ps1
+++ windows-10-debloat.ps1
@@ -30,7 +30,7 @@ else{
 
 $Form                            = New-Object system.Windows.Forms.Form
 $Form.ClientSize                 = New-Object System.Drawing.Point(1050,1000)
-$Form.text                       = "Windows Toolbox By Vijay J"
+$Form.text                       = "Windows Toolbox By YoYozaemon"
 $Form.StartPosition              = "CenterScreen"
 $Form.TopMost                    = $false
 $Form.BackColor                  = [System.Drawing.ColorTranslator]::FromHtml("#e9e9e9")
